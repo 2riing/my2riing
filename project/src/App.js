f538c9d3f3e6d9b01cd5c5a8ab4ec6731b6757df
2riing <5032303000@naver.com>
2023-03-22 20:48:09 +0900

    ibm sans font 적용 및 기본 레이아웃 잡기

--- project/src/App.js
+++ project/src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-
 import "./App.css";
 import ProfileHeader from "./component/ProfileHeader";
 import Introduce from "./component/Introduce";
--- project/src/component/ProfileHeader.jsx
+++ project/src/component/ProfileHeader.jsx
@@ -1,14 +1,40 @@
 import React from "react";
+import styled from "styled-components";
 
 function ProfileHeader(props) {
   return (
-    <div>
-      <div>Hi! THIS IS 2riing</div>
-      <div>사진</div>
-      <div>Profile</div>
-      <div>Contact</div>
-    </div>
+    <ProfileHeaderContainer>
+      <MyTitle>Hi! THIS IS 2riing!</MyTitle>
+      <MyInfo>
+        <div id="myPic">사진</div>
+        <div id="mySchool">Profile</div>
+        <div id="myContact">Contact</div>
+      </MyInfo>
+    </ProfileHeaderContainer>
   );
 }
+
+const ProfileHeaderContainer = styled.div``;
+const MyInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  #myPic {
+    width: 300px;
+  }
+  #mySchool {
+    width: 450px;
+  }
+  myContact {
+    width: 450px;
+  }
+`;
+const MyTitle = styled.div`
+  font-size: 60px;
+  font-weight: 900;
+  background: linear-gradient(to right top, #df2087, #ffa69e);
+  color: transparent;
+  -webkit-background-clip: text;
+  margin-bottom: 150px;
+`;
 
 export default ProfileHeader;
